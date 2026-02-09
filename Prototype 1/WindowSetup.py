187451dbd176818ee2c8f6da249bf0dc38d422d9
Kane Darkly <joadst@outlook.com>
2026-02-09 14:42:24 +0000

    Icon image fixed using file path

--- Prototype 1/WindowSetup.py
+++ Prototype 1/WindowSetup.py
@@ -5,7 +5,7 @@ def Create_Window():
     window.geometry("400x800") #sets the size of the window
     window.title("Prototype 1") #sets the title of the window
 
-    icon = PhotoImage(file="Logo.png")
+    icon = PhotoImage(file="C:/Users/Joads/OneDrive - Leeds Beckett University/Year 4/Dissertation/Prototype 1/Placeholder.png")
     window.iconphoto(True, icon) #sets the icon of the window
 
     window.mainloop() #Displays the window
